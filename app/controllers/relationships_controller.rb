77d13fe06215618a9a9703f2a45180c7c378c6e4
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2023-12-10 10:51:58 +0000

    [Update]フォロー機能 M.C

---
+++ app/controllers/relationships_controller.rb
@@ -0,0 +1,24 @@
+class RelationshipsController < ApplicationController
+  
+  # フォローする時
+  def create
+    current_user.follow(params[:user_id])
+    redirect_to request.referer
+  end
+  
+  # フォローを外す時
+  def destroy
+    current_user.unfollow(params[:user_id])
+    redirect_to request.referer
+  end
+  
+  def followings
+    user = User.find(params[:user_id])
+    @users = user.followings
+  end
+
+  def followers
+    user = User.find(params[:user_id])
+    @users = user.followers
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,13 @@ class User < ApplicationRecord
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
   has_many :book_comments, dependent: :destroy
+  # フォローをした、されたの関係
+  has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  # 一覧画面で使用
+  has_many :followings, through: :relationships, source: :followed
+  has_many :followers, through: :reverse_of_relationships, source: :follower
+  
   has_one_attached :profile_image
 
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
@@ -19,5 +26,21 @@ class User < ApplicationRecord
       profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
     end
       profile_image.variant(resize_to_limit: [width, height]).processed
+  end
+  
+  # フォローした時の処理
+  def follow(user_id)
+    relationships.create(followed_id: user_id)
+  end
+  
+  # フォローを外す時の処理
+  
+  def unfollow(user_id)
+    relationships.find_by(followed_id: user_id).destroy
+  end
+  
+  # フォローしているかの判定
+  def following?(user)
+    followings.include?(user)
   end
 end
