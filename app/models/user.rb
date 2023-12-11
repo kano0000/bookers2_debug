d59cf701aef9596155097ae918fd45d7464045af
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2023-12-11 05:51:17 +0000

    [Update]検索機能

--- app/models/user.rb
+++ app/models/user.rb
@@ -43,4 +43,17 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+  
+  # 検索
+  def self.search_for(content, method)
+    if method == 'perfect'
+      User.where(name: content)
+    elsif method == 'forward'
+      User.where('name LIKE ?', content + '%')
+    elsif method == 'backward'
+      User.where('name LIKE ?', '%' + content)
+    else
+      User.where('name LIKE ?', '%' + content + '%')
+    end
+  end
 end
