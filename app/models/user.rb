ef9de1c99d5e186c2d4e6bab9f56b2913d4badca
EC2 Default User <ec2-user@ip-172-31-7-67.ap-northeast-1.compute.internal>
2023-12-14 00:15:09 +0000

    [Update]DM,controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,7 @@ class User < ApplicationRecord
   # DM機能
   has_many :messages, dependent: :destroy
   has_many :entries, dependent: :destroy
+  has_many :rooms, through: :entries
 
   has_many :books, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -16,7 +17,7 @@ class User < ApplicationRecord
   # 一覧画面で使用
   has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
-  
+
   has_one_attached :profile_image
 
   validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
@@ -30,23 +31,23 @@ class User < ApplicationRecord
     end
       profile_image.variant(resize_to_limit: [width, height]).processed
   end
-  
+
   # フォローした時の処理
   def follow(user_id)
     relationships.create(followed_id: user_id)
   end
-  
+
   # フォローを外す時の処理
-  
+
   def unfollow(user_id)
     relationships.find_by(followed_id: user_id).destroy
   end
-  
+
   # フォローしているかの判定
   def following?(user)
     followings.include?(user)
   end
-  
+
   # 検索
   def self.search_for(content, method)
     if method == 'perfect'
